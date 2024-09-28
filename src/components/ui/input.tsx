fc2d84097c7817e2e3260fcf7501b1f718d2c4ec
tominaga <zmstmb2804@ymobile.ne.jp>
2024-09-28 23:00:20 +0900

    input.tsxの修正

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,12 +1,15 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  // 必要なプロパティを追加
+  label?: string; // 例: ラベル用プロパティ
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type = "text", ...props }, ref) => {
     return (
       <input
         type={type}
@@ -17,9 +20,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };

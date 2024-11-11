04340e779c87aa691f0f8a2f52ab0058cb54a729
tominaga <zmstmb2804@ymobile.ne.jp>
2024-11-11 13:51:35 +0900

    設定，プロフィール画面の簡易作成

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import { Header } from '@/components/Header';
+
+export default function Page() {
+    return (
+        <div>
+            <Header />
+            <h1>Hello, world!</h1>
+        </div>
+    );
+}
